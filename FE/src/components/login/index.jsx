eb840abf73693cc2626af787b53413a8e2fa66f4
shusbox <shusbox@naver.com>
2025-12-01 11:35:46 +0900

    feat: 로그인 백엔드 쿼리전송

--- FE/src/components/login/index.jsx
+++ FE/src/components/login/index.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import $ from "jquery";
 import * as AuthStyled from "../../styles/auth";
 import useLoginModalStore from "../../store/loginModal";
+import useSignupModalStore from "../../store/signupModal";
 
 const Login = () => {
   const { loginModal } = useLoginModalStore();
+  const { signupModal } = useSignupModalStore();
+
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  const onClickLogin = () => {
+    $.ajax({
+      type: 'POST',
+      url: '/signin',
+      daga: {
+        username: username,
+        password: password
+      }
+    });
+  };
 
   return (
     <>
@@ -10,16 +28,24 @@ const Login = () => {
         <AuthStyled.Title> 로그인 </AuthStyled.Title>
         <AuthStyled.Form>
           <AuthStyled.InputContainer>
-            <AuthStyled.Input placeholder="아이디" />
-            <AuthStyled.Input placeholder="비밀번호" />
+            <AuthStyled.Input
+              placeholder="아이디"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <AuthStyled.Input
+              placeholder="비밀번호"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </AuthStyled.InputContainer>
           <AuthStyled.ButtonContainer>
             <AuthStyled.ButtonSignup> 회원가입 </AuthStyled.ButtonSignup>
-            <AuthStyled.ButtonLogin> 로그인 </AuthStyled.ButtonLogin>
+            <AuthStyled.ButtonLogin onClick={onClickLogin}> 로그인 </AuthStyled.ButtonLogin>
           </AuthStyled.ButtonContainer>
         </AuthStyled.Form>
       </AuthStyled.Login>
-      <AuthStyled.Backdrop onClick={loginModal} />
+      <AuthStyled.Backdrop onClick={() => { loginModal(); signupModal(); }} />
     </>
   );
 };
