acd191aa2f79c7de70fc198d003ae3c9edcb4130
shusbox <shusbox@naver.com>
2025-12-09 16:51:05 +0900

    feat: 랭킹 쿼리 추가

--- FE/src/components/rank/list.jsx
+++ FE/src/components/rank/list.jsx
@@ -1,22 +1,30 @@
+import $ from "jquery";
+import { useState, useEffect } from "react";
 import * as RankStyled from "../../styles/rank";
 import Item from "./item";
 
 const List = () => {
+  const [ list, setList ] = useState([]);
+  const [ fail, setFail ] = useState(false);
+
+  useEffect(() => {
+    $.ajax({
+      type: 'GET',
+      url: 'http://127.0.0.1:5000/signin',
+    }).done((result) => {
+      setList(result);
+    }).fail((result) => {
+      console.log(result);
+      setFail(true);
+    })
+  }, []);
+
   return (
     <RankStyled.ListContainer>
-      <Item number={"1"} username={"장기하"} score={"8점"} />
-      <Item number={"1"} username={"장기하"} score={"8점"} />
-      <Item number={"1"} username={"장기하"} score={"8점"} />
-      <Item number={"1"} username={"장기하"} score={"8점"} />
-      <Item number={"1"} username={"장기하"} score={"8점"} />
-      <Item number={"1"} username={"장기하"} score={"8점"} />
-      <Item number={"1"} username={"장기하"} score={"8점"} />
-      <Item number={"1"} username={"장기하"} score={"8점"} />
-      <Item number={"1"} username={"장기하"} score={"8점"} />
-      <Item number={"1"} username={"장기하"} score={"8점"} />
-      <Item number={"1"} username={"장기하"} score={"8점"} />
-      <Item number={"1"} username={"장기하"} score={"8점"} />
-      <Item number={"1"} username={"장기하"} score={"8점"} />
+      {list.map((item, index) => {
+        <Item key={index} number={index + 1} username={item.username} score={item.score} />
+      })}
+      {fail && <p> 랭킹을 불러오지 못했습니다. </p> }
     </RankStyled.ListContainer>
   );
 };
