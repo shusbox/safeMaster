7fd66f39af4a39b478397307f4994564d1418a1e
shusbox <shusbox@naver.com>
2025-12-15 08:31:12 +0900

    feat: 회원가입 focus

--- FE/src/components/signup/index.jsx
+++ FE/src/components/signup/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import $ from "jquery";
 import useSignupModalStore from "../../store/signupModal";
@@ -7,10 +7,17 @@ import * as AuthStyled from "../../styles/auth";
 
 const Signup = () => {
   const navigate = useNavigate();
+
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+  const checkpasswordRef = useRef();
+
   const { signupModal } = useSignupModalStore();
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
   const [ checkPassword, setCheckPassword ] = useState('');
+  
+  const [ failed, setFailed ] = useState();
 
   const onClickSignup = (e) => {
     e.preventDefault();
@@ -23,13 +30,23 @@ const Signup = () => {
         password
       },
       contentType: 'application/x-www-form-urlencoded',
-    }).done((result) => {
-      console.log("회원가입 성공", result);
+    }).done(() => {
       navigate("/");
-    }).fail((result) => {
-      console.log(result);
+    }).fail(() => {
+      setFailed(true);
     })
   };
+
+  const handleKeyDown = (e) => {
+    if (failed) setFailed(ture);
+
+    if (e.key === 'Enter') {
+      e.preventDefault();
+
+      if (e.target.id === "username") passwordRef.current.focus();
+      if (e.target.id === "password") checkpasswordRef.current.focus();
+    }
+  }
 
   return (
     <>
@@ -38,12 +55,16 @@ const Signup = () => {
         <AuthStyled.Form>
           <AuthStyled.InputContainer>
             <AuthStyled.Input
+              ref={usernameRef}
+              id="username"
               placeholder="아이디"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
               required
             />
             <AuthStyled.Input
+              ref={passwordRef}
+              id="password"
               type="password"
               placeholder="비밀번호"
               value={password}
@@ -51,6 +72,8 @@ const Signup = () => {
               required
             />
             <AuthStyled.Input
+              ref={checkpasswordRef}
+              id="checkpassword"
               type="password"
               placeholder="비밀번호 확인"
               value={checkPassword}
@@ -58,6 +81,7 @@ const Signup = () => {
               required
             />
           </AuthStyled.InputContainer>
+          {failed && <AuthStyled.Failed> 회원가입에 실패하였습니다. </AuthStyled.Failed>}
           <AuthStyled.ButtonContainer>
             <AuthStyled.CancelButton onClick={signupModal}> 뒤로 </AuthStyled.CancelButton>
             <AuthStyled.SubmitButton onClick={onClickSignup}> 회원가입 </AuthStyled.SubmitButton>
